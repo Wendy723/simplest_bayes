bf61adb4e3c27353c91189ddb223d92f358a2e77
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-06 15:32:34 -0500

    add simple model

---
+++ models/models.R
@@ -0,0 +1,17 @@
+library(overseer)
+
+## note, if this script is going to be run interactively, make sure to set the
+## wd to the same location as the script itself
+## setwd(file.path(getwd()," models"))
+
+if (!interactive_model_check("one_cmt_iv.cpp")) {
+    stop("make sure the directory is set to the models directory before running interactively,
+         to make sure the relative paths will be the same as when sourcing")
+}
+
+models <- Overseer$new()
+
+
+models$add_model_file("one_cmt_iv")
+
+models
---
+++ models/one_cmt_iv.cpp
@@ -0,0 +1,34 @@
+[PARAM] @annotated
+CL  : 4   : Clearance (L/hr)
+V   : 35  : Volume (L) 
+
+
+[CMT] @annotated
+CENT : Central compartment (mg)
+
+[PKMODEL]
+ncmt=1, trans=11
+
+[MAIN]
+double CLi = CL*exp(nCL);
+double Vi = V*exp(nV);
+
+  
+[OMEGA] @annotated @correlation @block
+nCL : 0.1     : Random effect on CL
+nV  : 0.4 0.04 : Random effect on V
+    
+[SIGMA] @annotated
+PROP : 0.04 : Proportional error
+// so don't get into issues with estimating via multiplicative error only
+ADD  : 0.5 : Additive residual error
+
+[TABLE]
+double IPRED = CENT/Vi;
+double DV = CENT/Vi*(1+PROP) + ADD;
+
+[CAPTURE] @annotated
+DV    : plasma concentration (mg/L)
+IPRED : Individual predicted plasma concentration (mg/L)
+CLi   : Individual Clearance (L/hr)
+Vi    : Individual Volume (L)
