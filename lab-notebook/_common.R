1a97331e454f2493c9731092fb6086f027f1e269
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-09 12:15:31 -0500

    set autodep

--- lab-notebook/_common.R
+++ lab-notebook/_common.R
@@ -4,7 +4,8 @@ options(digits = 3)
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  cache = TRUE
+  cache = TRUE,
+  auto_dep = TRUE
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
