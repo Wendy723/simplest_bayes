fdc8b4bf7ad7ed5e5e86dc5109d3dfa08148b98f
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-17 18:22:19 -0500

    give verbose option so can debug derived sub folder creation if needed

--- scripts/save_derived_factory.R
+++ scripts/save_derived_factory.R
@@ -1,15 +1,19 @@
 #' factory function to save to derived location
 #' @param .derived_location folder to save output to
 #' @param .mkdir make directory of it does not exist, defaults to TRUE
+#' @param verbose whether to message when creating a new directory, defaults to FALSE
 #' @examples 
 #' save_derived <- save_derived_factory("../data/derived")
 #' save_derived(Theoph, "Theoph.rds")
-save_derived_factory <- function(.derived_location, .mkdir = TRUE) {
+save_derived_factory <- function(.derived_location, .mkdir = TRUE, verbose = FALSE) {
     .loc <- normalizePath(.derived_location)
     if (!dir.exists(.loc)) {
         if (!.mkdir) {
             stop(paste("could not find directory at location: ", .derived_location))
         } else {
+            if (verbose) {
+                message('new directory created at: ', .loc)
+            }
             dir.create(.loc, recursive = T) 
         }
     }
