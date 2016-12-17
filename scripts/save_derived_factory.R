b0550609f0c5bb72cb5cc14c010af100e1b0b402
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-17 18:19:43 -0500

    also create directories if dont exist to save files to

--- scripts/save_derived_factory.R
+++ scripts/save_derived_factory.R
@@ -1,9 +1,18 @@
 #' factory function to save to derived location
+#' @param .derived_location folder to save output to
+#' @param .mkdir make directory of it does not exist, defaults to TRUE
 #' @examples 
 #' save_derived <- save_derived_factory("../data/derived")
 #' save_derived(Theoph, "Theoph.rds")
-save_derived_factory <- function(.derived_location) {
+save_derived_factory <- function(.derived_location, .mkdir = TRUE) {
     .loc <- normalizePath(.derived_location)
+    if (!dir.exists(.loc)) {
+        if (!.mkdir) {
+            stop(paste("could not find directory at location: ", .derived_location))
+        } else {
+            dir.create(.loc, recursive = T) 
+        }
+    }
    return(function(.o, filename, ...) {
        saveRDS(.o, file.path(.loc, filename, ...))
    }) 
